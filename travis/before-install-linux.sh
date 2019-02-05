7b8034e789068629ce2698f8ab76e206a4f7db8b
njlr <njlr@users.noreply.github.com>
2019-02-05 18:20:43 +0000

    * Bumps deps

--- travis/before-install-linux.sh
+++ travis/before-install-linux.sh
@@ -9,10 +9,10 @@ gcc --version
 
 mkdir -p bin
 
-wget -N https://github.com/njlr/buck-warp/releases/download/v0.2.0/buck-2019.01.10.01-linux -O bin/buck
+wget -c https://github.com/njlr/buck-warp/releases/download/v0.2.0/buck-2019.01.10.01-linux -O bin/buck
 chmod +x ./bin/buck
 ./bin/buck --version
 
-wget -N https://github.com/LoopPerfect/buckaroo/releases/download/$BUCKAROO_VERSION/buckaroo-linux -O bin/buckaroo
+wget -c https://github.com/LoopPerfect/buckaroo/releases/download/$BUCKAROO_VERSION/buckaroo-linux -O bin/buckaroo
 chmod +x ./bin/buckaroo
 ./bin/buckaroo version
--- travis/before-install-osx.sh
+++ travis/before-install-osx.sh
@@ -6,10 +6,10 @@ gcc --version
 
 mkdir -p bin
 
-wget -N https://github.com/njlr/buck-warp/releases/download/v0.2.0/buck-2019.01.10.01-osx -O bin/buck
+wget -c https://github.com/njlr/buck-warp/releases/download/v0.2.0/buck-2019.01.10.01-osx -O bin/buck
 chmod +x ./bin/buck
 ./bin/buck --version
 
-wget -N https://github.com/LoopPerfect/buckaroo/releases/download/$BUCKAROO_VERSION/buckaroo-macos -O bin/buckaroo
+wget -c https://github.com/LoopPerfect/buckaroo/releases/download/$BUCKAROO_VERSION/buckaroo-macos -O bin/buckaroo
 chmod +x ./bin/buckaroo
 ./bin/buckaroo version
